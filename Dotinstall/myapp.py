9252fee873b86fd24e3e12ef8dd8f5e3f8cc097b
片川良平 <kyatakawa1230@gmail.com>
2021-08-09 21:09:48 +0900

    seleniumのsetup

---
+++ Dotinstall/myapp.py
@@ -0,0 +1,5 @@
+# print([i for i in range(10)])
+# print([i * 3 for i in range(10) if i % 2 == 0])
+# print((i * 3 for i in range(10) if i % 2 == 0))
+print(i * 3 for i in range(10) if i % 2 == 0)
+print({i * 3 for i in range(10) if i % 2 == 0})
